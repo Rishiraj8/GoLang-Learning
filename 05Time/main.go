3d30cc82a695802e5985a550e22ca68eb7254fd0
Rishi <rishiraj.nr2022cse@sece.ac.in>
2024-09-22 13:02:26 +0530

    about Time formating "so diff than other langs" with their default time ref

---
+++ 05Time/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Gonna print the current time")
+	presentTime := time.Now()
+
+	fmt.Println(presentTime.Format("Monday Jan"))
+	// format for printing what you need Basic full date  "Mon Jan 2 15:04:05 MST 2006" gives "Wed Feb 25 11:06:39 PST 2015"
+	//example we have to defaulty type monday to have the day printing
+	fmt.Println("The current time only is : ", presentTime.Format("15:04:05"))
+	createdTime := time.Date(2004, time.December, 8, 0, 0, 0, 0, time.Now().Location())
+	fmt.Println(createdTime)
+
+}
