433379f3affbf3c1f3328ee6c94fe277282415c8
Rishi <rishiraj.nr2022cse@sece.ac.in>
2024-12-26 07:17:28 +0530

    Add methods and functions for book details in Go example

--- 12Struct/main.go
+++ 12Struct/main.go
@@ -8,6 +8,22 @@ type book struct {
 	author string
 	pages int
 }
+// Method associated with the `book` type
+//see the difference between the function and method
+//this is method
+func (b book) details() {
+    fmt.Printf("'%s' by %s, %d pages\n", b.title, b.author, b.pages)
+}
+
+//this is function
+func printDetails(title string, author string, pages int) {
+    fmt.Printf("'%s' by %s, %d pages\n", title, author, pages)
+}
+// Why Use Methods?
+// Encapsulation: Methods provide a cleaner, more intuitive way to operate on a type and its fields.
+// Reusability: You don't need to pass the fields of the type explicitly—methods can directly access them.
+// Readability: Code like book1.details() is more natural and readable than calling a standalone function with arguments.
+
 func main()  {
 	//but here we have to follow the order of the struct
 	// and also put commos between the fields
@@ -19,4 +35,6 @@ func main()  {
         author: "Paulo Coelho",
     }
 	fmt.Println(book2)
+	book1.details()
+	printDetails(book2.title, book2.author, book2.pages)
 }
